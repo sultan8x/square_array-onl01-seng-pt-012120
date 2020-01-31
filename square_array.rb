49ef231ca7e42122dd59ea971aeb39f17561f35b
Sultan Afsheen <leadsultan8x@gmail.com>
2020-01-31 19:54:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,6 @@
 
 def square_array(array)
+<<<<<<< HEAD
     squared_array = []
 
     array.each { |any| any ** 2}
@@ -13,3 +14,9 @@ def square_array(array)
 
     return squared_array
   end
+=======
+  array.each do |i|
+    i(i ** 2)
+  end
+end
+>>>>>>> 18b771ffc7b22094748b4e0807701964ee3cd43d
